5d4568c032fed61a814d3ffd5b099d41e19b4613
Miraclev1 <127039117+Miracleprjkt@users.noreply.github.com>
2024-04-07 16:36:41 +0700

    Update ginkgo_build_script.sh

--- scripts/ginkgo_build_script.sh
+++ scripts/ginkgo_build_script.sh
@@ -1,146 +1,66 @@
-#!/usr/bin/env bash
+#!/bin/bash
+#
+# Compile script for QuicksilveR kernel
+# Copyright (C) 2020-2021 Adithya R.
 
-# Dependencies
-git clone $REPO -b $BRANCH kernel 
-cd kernel
+SECONDS=0 # builtin bash timer
+ZIPNAME="Emperor-Kernel_Ginkgo.zip"
+TC_DIR="$HOME/tc/azure-clang"
+AK3_DIR="$HOME/android/AnyKernel3"
+DEFCONFIG="vendor/ginkgo-perf_defconfig"
 
-clang() {
-    echo "Cloning clang"
-    if [ ! -d "clang" ]; then
-        git clone https://github.com/kdrag0n/proton-clang clang --depth=1
-        KBUILD_COMPILER_STRING="Proton clang 13.0 x sirnewbies"
-        PATH="${PWD}/clang/bin:${PATH}"
-    fi
-    sudo apt install -y ccache
-    echo "Done"
-}
+export PATH="$TC_DIR/bin:$PATH"
 
-IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-DATE=$(date +"%Y%m%d-%H%M")
-START=$(date +"%s")
-KERNEL_DIR=$(pwd)
-CACHE=1
-export CACHE
-export KBUILD_COMPILER_STRING
-ARCH=arm64
-export ARCH
-KBUILD_BUILD_HOST="Miraclev1"
-export KBUILD_BUILD_HOST
-KBUILD_BUILD_USER="Emperor"
-export KBUILD_BUILD_USER
-DEVICE="Redmi Note 8"
-export DEVICE
-CODENAME="ginkgo"
-export CODENAME
-DEFCONFIG="vendor/trinket-perf_defconfig"
-export DEFCONFIG
-COMMIT_HASH=$(git rev-parse --short HEAD)
-export COMMIT_HASH
-PROCS=$(nproc --all)
-export PROCS
-STATUS=STABLE
-export STATUS
-source "${HOME}"/.bashrc && source "${HOME}"/.profile
-if [ $CACHE = 1 ]; then
-    ccache -M 100G
-    export USE_CCACHE=1
+if ! [ -d "$TC_DIR" ]; then
+echo "Azure clang not found! Cloning to $TC_DIR..."
+if ! git clone -q -b main --depth=1 https://gitlab.com/Panchajanya1999/azure-clang $TC_DIR; then
+echo "Cloning failed! Aborting..."
+exit 1
 fi
-LC_ALL=C
-export LC_ALL
+fi
 
-tg() {
-    curl -sX POST https://api.telegram.org/bot"${token}"/sendMessage -d chat_id="${chat_id}" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null
-}
+export KBUILD_BUILD_USER=Miraclev1
+export KBUILD_BUILD_HOST=Emperor-Kernel
 
-tgs() {
-    MD5=$(md5sum "$1" | cut -d' ' -f1)
-    curl -fsSL -X POST -F document=@"$1" https://api.telegram.org/bot"${token}"/sendDocument \
-        -F "chat_id=${chat_id}" \
-        -F "parse_mode=Markdown" \
-        -F "caption=$2 | *MD5*: \`$MD5\`"
-}
+if [[ $1 = "-r" || $1 = "--regen" ]]; then
+make O=out ARCH=arm64 $DEFCONFIG savedefconfig
+cp out/defconfig arch/arm64/configs/$DEFCONFIG
+exit
+fi
 
-# Sticker
-sticker() {
-    curl -s -X POST https://api.telegram.org/bot"${token}"/sendSticker \
-        -d sticker="CAACAgUAAxkBAAEKwfdlVQiHDyeU33wu71hiEWMeemBHhAACTAUAAgjXqFdZ0rpx15brLTME" \
-        -d chat_id="${chat_id}"
-}
+if [[ $1 = "-c" || $1 = "--clean" ]]; then
+rm -rf out
+fi
 
-# Error Sticker
-error_sticker() {
-    curl -s -X POST https://api.telegram.org/bot"${token}"/sendSticker \
-        -d sticker="CAACAgQAAxkBAAEKxCplVkvohpIHdto0Sq0FNnsjpiFN6AACQAoAAmfRmFBaWxhVsThPHDME" \
-        -d chat_id="${chat_id}"
-}
+mkdir -p out
+make O=out ARCH=arm64 $DEFCONFIG
 
-# Send Build Info
-sendinfo() {
-    tg "
-• sirCompiler Action •
-*Building on*: \`Github actions\`
-*Date*: \`${DATE}\`
-*Device*: \`${DEVICE} (${CODENAME})\`
-*Branch*: \`$(git rev-parse --abbrev-ref HEAD)\`
-*Last Commit*: [${COMMIT_HASH}](${REPO}/commit/${COMMIT_HASH})
-*Compiler*: \`${KBUILD_COMPILER_STRING}\`
-*Build Status*: \`${STATUS}\`"
-    sticker
-}
+echo -e "\nStarting compilation...\n"
+make -j$(nproc --all) O=out ARCH=arm64 CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-gnu- CROSS_COMPILE_ARM32=arm-linux-gnueabi- Image.gz-dtb dtbo.img
 
-# Push kernel to channel
-push() {
-    cd AnyKernel || exit 1
-    ZIP=$(echo *.zip)
-    tgs "${ZIP}" "Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s). | For *${DEVICE} (${CODENAME})* | ${KBUILD_COMPILER_STRING}"
-}
-
-# Catch Error
-finderr() {
-    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
-        -d chat_id="$chat_id" \
-        -d "disable_web_page_preview=true" \
-        -d "parse_mode=markdown" \
-        -d sticker="CAACAgIAAxkBAAED3JViAplqY4fom_JEexpe31DcwVZ4ogAC1BAAAiHvsEs7bOVKQsl_OiME" \
-        -d text="Build throw an error(s)"
-    error_sticker
-    exit 1
-}
-
-# Compile
-compile() {
-
-    if [ -d "out" ]; then
-        rm -rf out && mkdir -p out
-    fi
-
-    make O=out ARCH="${ARCH}" "${DEFCONFIG}"
-    make -j"${PROCS}" O=out \
-        ARCH=$ARCH \
-        CC="clang" \
-        LLVM=1 \
-        CROSS_COMPILE=aarch64-linux-gnu- \
-        CROSS_COMPILE_ARM32=arm-linux-gnueabi-
-
-    if ! [ -a "$IMAGE" ]; then
-        finderr
-        exit 1
-    fi
-
-    git clone --depth=1 https://github.com/sirnewbies/Anykernel3.git AnyKernel -b tissot
-    cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
-}
-# Zipping
-zipping() {
-    cd AnyKernel || exit 1
-    zip -r9 Another-Kernel-"${BRANCH}"-"${CODENAME}"-"${DATE}".zip ./*
-    cd ..
-}
-
-clang
-sendinfo
-compile
-zipping
-END=$(date +"%s")
-DIFF=$((END - START))
-push
+if [ -f "out/arch/arm64/boot/Image.gz-dtb" ] && [ -f "out/arch/arm64/boot/dtbo.img" ]; then
+echo -e "\nKernel compiled succesfully! Zipping up...\n"
+if [ -d "$AK3_DIR" ]; then
+cp -r $AK3_DIR AnyKernel3
+elif ! git clone -q https://github.com/Miracleprjkt/AnyKernel3; then
+echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
+exit 1
+fi
+cp out/arch/arm64/boot/Image.gz-dtb AnyKernel3
+cp out/arch/arm64/boot/dtbo.img AnyKernel3
+rm -f *zip
+cd AnyKernel3
+git checkout master &> /dev/null
+zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+cd ..
+rm -rf AnyKernel3
+rm -rf out/arch/arm64/boot
+echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
+echo "Zip: $ZIPNAME"
+if ! [[ $HOSTNAME = "Emperor-kernel" && $USER = "Miraclev1" ]]; then
+curl --upload-file $ZIPNAME http://transfer.sh/$ZIPNAME; echo
+fi
+else
+echo -e "\nCompilation failed!"
+exit 1
+fi
